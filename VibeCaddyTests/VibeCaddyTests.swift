764f6b2db31931d4ff598cdde1a587c725becd0b
Jonathan Corpuz <jonocorpuz@gmail.com>
2026-09-02 18:34:27 -0700

    Initial Commit

---
+++ VibeCaddyTests/VibeCaddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCaddyTests.swift
+//  VibeCaddyTests
+//
+//  Created by Jonathan Corpuz on 2026-09-02.
+//
+
+import Testing
+@testable import VibeCaddy
+
+struct VibeCaddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
